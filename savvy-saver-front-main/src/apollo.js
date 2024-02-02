6f45fe3b8f050c4f0f911158d5c5ef36aba3bc4b
Isabella Santos <bellacristsantos@gmail.com>
2024-02-02 16:54:41 -0300

    fix: LearnMore component

---
+++ savvy-saver-front-main/src/apollo.js
@@ -0,0 +1,26 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+import { setContext } from '@apollo/client/link/context';
+
+
+
+const httpLink = createHttpLink({
+  uri: 'https://api.taddy.org',
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      'X-USER-ID': " ",
+      'X-API-KEY': "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default client;
