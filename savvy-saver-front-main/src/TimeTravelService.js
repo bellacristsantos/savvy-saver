b66b2847baf01c2236b053bec52d5b88c96cee1d
Rodwell St Clair <rodwell.st.clair592@gmail.com>
2024-02-02 09:07:23 +0000

    fixed the calender and added the searchlist but didnt connect to FE, it makes many API request

--- savvy-saver-front-main/src/TimeTravelService.js
+++ savvy-saver-front-main/src/TimeTravelService.js
@@ -19,3 +19,32 @@ export async function fetchStockData(company) {
   }
 
 }
+
+export async function fetchCompanyName(company) {
+  const API_KEY = '';
+  const URL = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${company}&apikey=${API_KEY}`;
+
+  try {
+    const response = await fetch(URL, {
+      method: 'GET',
+      headers: {
+        'User-Agent': 'request',
+        'Content-Type': 'application/json'
+      },
+    })
+    const data = await response.json();
+    console.log(data);
+    const companyList = [];
+    for (const key in data.bestMatches) {
+      const company = data.bestMatches[key];
+      companyList.push({
+        'symbol': company['1. symbol'],
+        'name': company['2. name']
+      });
+    }
+    return companyList;
+  }
+  catch (error) {
+    console.log('Error with fetchCompanyName Funct', error);
+  }
+}
---
+++ savvy-saver-front-main/src/components/TimeTravel/CompanyList.js
@@ -0,0 +1,12 @@
+function CompanyList({ companylist }) {
+  return (
+    <div
+      style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}
+    >
+      <div>companylist.symbol</div>
+      <div>companylist.name</div>
+    </div>
+  )
+}
+
+export default CompanyList
